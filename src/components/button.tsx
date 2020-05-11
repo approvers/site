2c5df4a73e3113ecfb97359877b359e6a98dd7b8
MikuroXina <ryosukadnak@gmail.com>
2020-05-11 12:12:38 +0900

    Fix to foward ref

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,33 +1,37 @@
-import {ReactNode, FC, Ref, MouseEvent} from 'react';
+import {ReactNode, FC, Ref, MouseEvent, forwardRef, RefObject} from 'react';
 
-export const ShadowButton: FC<{
+type ShadowButtonProps = {
   children: ReactNode;
   onClick?: (event: MouseEvent<HTMLDivElement>) => void;
-  ref?: Ref<HTMLDivElement>;
-}> = ({ref, onClick, children}) => (
-  <>
-    <div ref={ref} onClick={onClick}>
-      {children}
-    </div>
-    <style jsx>{`
-      div {
-        min-width: 6rem;
-        padding: 1rem 0;
-        margin: 0.2rem;
-        border: solid black 1px;
-        border-radius: 4px;
-        box-shadow: 0.2rem 0.2rem 0.2rem rgba(0, 0, 0, 0.4);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-      }
-      div :hover {
-        box-shadow: 0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.4);
-      }
-      div :active {
-        box-shadow: 0rem 0rem 0rem rgba(0, 0, 0, 0.4);
-      }
-    `}</style>
-  </>
+  ref?: RefObject<HTMLDivElement>;
+};
+
+export const ShadowButton: FC<ShadowButtonProps> = forwardRef(
+  ({onClick, children}: ShadowButtonProps, ref?: Ref<HTMLDivElement>) => (
+    <>
+      <div ref={ref} onClick={onClick}>
+        {children}
+      </div>
+      <style jsx>{`
+        div {
+          min-width: 6rem;
+          padding: 1rem 0;
+          margin: 0.2rem;
+          border: solid black 1px;
+          border-radius: 4px;
+          box-shadow: 0.2rem 0.2rem 0.2rem rgba(0, 0, 0, 0.4);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+        }
+        div :hover {
+          box-shadow: 0.1rem 0.1rem 0.1rem rgba(0, 0, 0, 0.4);
+        }
+        div :active {
+          box-shadow: 0rem 0rem 0rem rgba(0, 0, 0, 0.4);
+        }
+      `}</style>
+    </>
+  )
 );
