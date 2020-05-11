983b7db62093d112625fb07d8524a2d145e70292
Mikuro Xina <ryosukadnak@gmail.com>
2020-05-11 19:11:08 +0900

    Lint (#4)

--- src/lib/blog-fetch.ts
+++ src/lib/blog-fetch.ts
@@ -4,19 +4,23 @@ import matter from "gray-matter";
 
 const postsDirectory = path.join(process.cwd(), "src/blogs");
 
+export type BlogPostId = string;
+
 export type Metadata = {
+  id: BlogPostId;
   date: string;
   title: string;
 };
 
 export type Blog = {
-  id: string;
   content: string;
 } & Metadata;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const validateMetadata = (value: any): value is Metadata => {
   return (
+    "id" in value &&
+    typeof value.id === "string" &&
     "date" in value &&
     typeof value.date === "string" &&
     "title" in value &&
@@ -24,7 +28,7 @@ const validateMetadata = (value: any): value is Metadata => {
   );
 };
 
-const metadataFromFile = (fileName: string) => {
+const metadataFromFile = (fileName: string): Metadata => {
   const id = fileName.replace(/\.md$/, "");
   const fullPath = path.join(postsDirectory, fileName);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -52,7 +56,7 @@ export function getSortedBlogMetadatas(): Metadata[] {
   });
 }
 
-export async function getAllBlogIds() {
+export async function getAllBlogIds(): Promise<{ params: { id: BlogPostId } }[]> {
   const fileNames = await new Promise<string[]>((resolve, reject) =>
     fs.readdir(postsDirectory, (e, files) => (e ? reject(e) : resolve(files))),
   );
@@ -64,7 +68,7 @@ export async function getAllBlogIds() {
   }));
 }
 
-export async function getBlogFromId(id: string) {
+export async function getBlogFromId(id: string): Promise<Blog> {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,91 +1,92 @@
 import Head from "next/head";
 import { Header } from "../components/header";
 import { Layout } from "../components/layout";
+import { NextPage } from "next";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>限界開発鯖</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Header />
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>限界開発鯖</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Layout>
+      <Header />
 
-        <main>
-          <h1 className="title">
-            <em>限界開発鯖</em> へようこそ！
-          </h1>
+      <main>
+        <h1 className="title">
+          <em>限界開発鯖</em> へようこそ！
+        </h1>
 
-          <p className="subtitle">Over Limit Development</p>
-        </main>
+        <p className="subtitle">Over Limit Development</p>
+      </main>
 
-        <footer>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-          </a>
-        </footer>
-      </Layout>
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+    </Layout>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    <style jsx>{`
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      footer {
+        width: 100%;
+        height: 100px;
+        border-top: 1px solid #eaeaea;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+      footer img {
+        margin-left: 0.5rem;
+      }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+      footer a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+      .title {
+        margin: 0;
+        line-height: 1.15;
+        font-size: 4rem;
+      }
 
-        .title,
-        .subtitle {
-          text-align: center;
-        }
+      .title,
+      .subtitle {
+        text-align: center;
+      }
 
-        .subtitle {
-          line-height: 1.5;
-          font-style: italic;
-          font-size: 1.5rem;
-        }
+      .subtitle {
+        line-height: 1.5;
+        font-style: italic;
+        font-size: 1.5rem;
+      }
 
-        .logo {
-          height: 1em;
-        }
-      `}</style>
-    </>
-  );
-}
+      .logo {
+        height: 1em;
+      }
+    `}</style>
+  </>
+);
+
+export default Home;
