b3fd33775ca1be940a0b49a63c00603466dd7465
MikuroXina <ryosukadnak@gmail.com>
2020-05-11 13:02:21 +0900

    Add JoinPage

---
+++ src/pages/join.tsx
@@ -0,0 +1,22 @@
+import {NextPage} from 'next';
+import {Header} from '../components/header';
+import {Layout} from '../components/layout';
+
+const JoinPage: NextPage = () => (
+  <>
+    <Layout>
+      <Header />
+      <main>
+        <h1>参加方法</h1>
+        <section>
+          <p>弊サーバーは、完全紹介制となっています。</p>
+          <p>
+            知り合いのメンバーに連絡を取って、招待リンクを受け取ってください。
+          </p>
+        </section>
+      </main>
+    </Layout>
+  </>
+);
+
+export default JoinPage;
