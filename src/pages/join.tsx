eaebc7b8b8c59ee658b9cd5fcf1cee4df4b74d86
unios103 <unios103@gmail.com>
2020-05-14 21:51:18 +0900

    参加方法ページのレイアウト等

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -1,16 +1,18 @@
 import { NextPage } from "next";
 import { Header } from "../components/header";
 import { Layout } from "../components/layout";
-import styles from "../sass/wrapper.module.sass";
+import styles from "../sass/pages/join.module.sass";
 
 const JoinPage: NextPage = () => (
   <>
     <Layout>
       <Header />
-      <main className={styles.mainContents}>
-        <h1>参加方法</h1>
-        <section>
-          <p>弊サーバーは、完全紹介制となっています。</p>
+      <main className={styles.joinPage}>
+        <h1 className={styles.title}>参加方法</h1>
+        <section className={styles.joinText}>
+          <p>
+            弊サーバーは、<span className={styles.color}>完全紹介制</span>となっています。
+          </p>
           <p>知り合いのメンバーに連絡を取って、招待リンクを受け取ってください。</p>
         </section>
       </main>
