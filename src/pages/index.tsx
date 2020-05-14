efa02f74ed89b82e9231879004158d683cc56719
unios103 <unios103@gmail.com>
2020-05-14 21:06:19 +0900

    ホームページのレイアウト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Header } from "../components/header";
-import { Layout } from "../components/layout";
 import { NextPage } from "next";
 import styles from "../sass/pages/index.module.sass";
 
@@ -10,15 +9,39 @@ const Home: NextPage = () => (
       <title>限界開発鯖</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Layout>
+    <div className={styles.homeWrapper}>
       <Header />
       <main className={styles.homeMainContents}>
         <h1 className={styles.title}>
           <em>†限界開発鯖†</em> へようこそ！
         </h1>
-        <p className={styles.subtitle}>Over Limit Development</p>
+        <h2 className={styles.subtitle}>Over Limit Development</h2>
+        <div>
+          <section className={styles.aboutUs}>
+            <h3>
+              <span>Q</span>限界開発鯖ってなに?
+            </h3>
+            <p>
+              <span>A</span>限界開発鯖は限界高専生が限界状態で開発を行っているDiscord鯖です。
+            </p>
+          </section>
+          <section className={styles.aboutUs}>
+            <h3>
+              <span>Q</span>限界開発鯖の雰囲気が知りたい！
+            </h3>
+            <p>
+              <span>A</span>こちらへどうぞ →{" "}
+              <a
+                href="https://twitter.com/search?q=%23限界開発鯖&src=typed_query"
+                className={styles.genkaiLink}
+              >
+                #限界開発鯖
+              </a>
+            </p>
+          </section>
+        </div>
       </main>
-    </Layout>
+    </div>
   </>
 );
 
