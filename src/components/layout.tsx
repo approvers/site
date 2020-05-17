0ca413583f235320c24f9f3eeb9be990948b8fb3
shun-shobon <shun.shobon@gmail.com>
2020-05-17 15:33:14 +0900

    feat: add favicons

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,6 +28,14 @@ export const Layout: FC<{ pageName: string; description?: string; children: Reac
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={siteImage} />
       <meta name="twitter:card" content="summary_large_image" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href={"/apple-touch-icon.png"} />
+      <link rel="icon" type="image/png" sizes="32x32" href={"/favicon-32x32.png"} />
+      <link rel="icon" type="image/png" sizes="16x16" href={"/favicon-16x16.png"} />
+      <link rel="manifest" href={"/site.webmanifest"} />
+      <link rel="mask-icon" href={"/safari-pinned-tab.svg"} color="#000000" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#000000" />
     </Head>
     <div className={styles.wrapper}>{children}</div>
   </div>
