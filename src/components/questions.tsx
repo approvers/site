64ff2e6239c6b9fd88822813e19951f7ff90b536
MikuroXina <ryosukadnak@gmail.com>
2020-05-20 17:38:49 +0900

    Add QA component

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -1,30 +1,40 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { ExternalLink } from "./externalLink";
 import styles from "../sass/pages/index.module.sass";
 
+const QA: FC<{ question: ReactNode; answer: ReactNode }> = ({ question, answer }) => (
+  <section className={styles.aboutUs}>
+    <p>
+      <span>Q.</span>
+      {question}
+    </p>
+    <p>
+      <span>A.</span>
+      {answer}
+    </p>
+  </section>
+);
+
 export const Questions: FC = () => (
   <div>
-    <section className={styles.aboutUs}>
-      <p>
-        <span>Q</span>限界開発鯖ってなに?
-      </p>
-      <p>
-        <span>A</span>限界開発鯖は限界高専生が限界状態で開発を行っているDiscord鯖です。
-      </p>
-    </section>
-    <section className={styles.aboutUs}>
-      <p>
-        <span>Q</span>限界開発鯖の雰囲気が知りたい！
-      </p>
-      <p>
-        <span>A</span>こちらへどうぞ &rarr;{" "}
-        <ExternalLink
-          href="https://twitter.com/search?q=%23限界開発鯖&src=typed_query"
-          className={styles.genkaiLink}
-        >
-          #限界開発鯖
-        </ExternalLink>
-      </p>
-    </section>
+    <QA
+      question={<>限界開発鯖ってなに?</>}
+      answer={<>限界開発鯖は限界高専生が限界状態で開発を行っているDiscord鯖です。</>}
+    />
+
+    <QA
+      question={<>限界開発鯖の雰囲気が知りたい！</>}
+      answer={
+        <>
+          こちらへどうぞ &rarr;{" "}
+          <ExternalLink
+            href="https://twitter.com/search?q=%23限界開発鯖&src=typed_query"
+            className={styles.genkaiLink}
+          >
+            #限界開発鯖
+          </ExternalLink>
+        </>
+      }
+    />
   </div>
 );
