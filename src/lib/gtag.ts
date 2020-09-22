28bcb804e969287aad9cd9adc4769eed51ef5e37
shun-shobon <me@shun.technology>
2020-09-22 22:35:18 +0900

    feat: add gtag utils

---
+++ src/lib/gtag.ts
@@ -0,0 +1,22 @@
+export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID ?? "";
+
+export const pageview = (url: string): void => {
+  window.gtag("config", GOOGLE_ANALYTICS_ID, {
+    page_path: url,
+  });
+};
+
+type Event = {
+  action: string;
+  category: string;
+  label: string;
+  value?: string;
+};
+
+export const event = ({ action, category, label, value }: Event): void => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
