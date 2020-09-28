d09cd360dc42a2419ca2582808feab97fd560b7c
shun-shobon <me@shun.technology>
2020-09-28 22:02:20 +0900

    refactor: change environment variables

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID ?? "";
+export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? "";
 
 export const pageview = (url: string): void => {
   window.gtag("config", GOOGLE_ANALYTICS_ID, {
