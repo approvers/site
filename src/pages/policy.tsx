bbb989534d0f92361bf2a30e0092edbb01f794ba
MikuroXina <ryosukadnak@gmail.com>
2020-10-16 20:35:28 +0900

    :art: Integrate indenting

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -7,16 +7,16 @@ const PolicyPage: NextPage = () => (
   <Layout pageName="限界開発鯖 - プライバシーポリシー">
     <h1 className={styles.title}>プライバシーポリシー</h1>
     <section className={styles.policyText}>
-      <p className={styles.textindent1em}>当サイトは Google アナリティクスを使用しております。</p>
+      <p>当サイトは Google アナリティクスを使用しております。</p>
 
-      <p className={styles.textindent1em}>
+      <p>
         取得するデータは当サイトの改善に役立てる事のみに使用するとし、
         <span className={styles.color}>
           個人の特定やその他個人の不利益に関わることは致しません。
         </span>
       </p>
 
-      <p className={styles.textindent1em}>
+      <p>
         規約の詳細に関しましては、
         <ExternalLink
           href="https://marketingplatform.google.com/about/analytics/terms/jp/"
