a25057bf24b4d74e86cbdafe7ba414144cacf69c
MikuroXina <ryosukadnak@gmail.com>
2020-10-17 00:37:01 +0900

    :fire: Make copiright more short

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,6 +7,6 @@ export const Footer: FC = () => (
     <Link href="/policy">
       <a className={styles.footerLink}>プライバシーポリシー</a>
     </Link>{" "}
-    - Copyright (c) 2020 Approvers, all rights reserved.
+    - (c) 2020 Approvers
   </footer>
 );
