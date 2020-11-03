002dd7c89645daf772ff01886c1879e1111e7eb2
MikuroXina <ryosukadnak@gmail.com>
2020-11-03 20:04:02 +0900

    💄 Change title to splash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import { NextPage } from "next";
 import { Questions } from "../components/questions";
 import styles from "../scss/pages/index.module.scss";
 import { Layout } from "../components/layout";
+import { Subtitle } from "../components/title";
 
 const Home: NextPage = () => (
   <Layout pageName="限界開発鯖">
-    <h1 className={styles.title}>
+    <h1 className={styles.splash}>
       <em>&dagger;限界開発鯖&dagger;</em> へようこそ！
     </h1>
-    <h2 className={styles.subtitle}>Over Limit Development</h2>
+    <Subtitle>Over Limit Development</Subtitle>
     <Questions />
   </Layout>
 );
