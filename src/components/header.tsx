b0819aa6648bc705e7526e299d11bc0edba73dca
MikuroXina <ryosukadnak@gmail.com>
2020-12-19 23:57:22 +0900

    🎨 Split Links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,16 +21,23 @@ const links: { name: string; url: string }[] = [
     url: "/link",
   },
 ];
+
+const Links: FC = () => (
+  <>
+    {links.map(({ name, url }) => (
+      <Link key={name} href={url}>
+        <Button>
+          <a>{name}</a>
+        </Button>
+      </Link>
+    ))}
+  </>
+);
+
 export const Header: FC = () => (
   <header className={styles.headerLayout}>
     <div className={styles.buttonWrapper}>
-      {links.map(({ name, url }) => (
-        <Link key={name} href={url}>
-          <Button>
-            <a>{name}</a>
-          </Button>
-        </Link>
-      ))}
+      <Links />
     </div>
   </header>
 );
