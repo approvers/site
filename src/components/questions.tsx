be7dc251bbd86cb1306139c6246d900f04a6871f
MikuroXina <ryosukadnak@gmail.com>
2020-12-20 00:12:27 +0900

    🎨 Split questionAnswers

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -15,32 +15,37 @@ const QA: FC<{ question: ReactNode; answer: ReactNode }> = ({ question, answer }
   </section>
 );
 
+const questionAnswers = [
+  {
+    question: "限界開発鯖ってなに?",
+    answer: "限界開発鯖は高専生を中心に限界になった人々が限界状態で開発を行う Discord 鯖です。",
+  },
+  {
+    question: "限界開発鯖の雰囲気が知りたい！",
+    answer: (
+      <>
+        こちらへどうぞ &rarr;{" "}
+        <ExternalLink href="https://twitter.com/search?q=%23限界開発鯖&src=typed_query">
+          #限界開発鯖
+        </ExternalLink>
+      </>
+    ),
+  },
+  {
+    question: "どうやって参加するの？",
+    answer: (
+      <>
+        弊サーバーは、<span className={styles.color}>完全紹介制</span>です。
+        知り合いのメンバーから招待リンクを受け取ってください。
+      </>
+    ),
+  },
+] as const;
+
 export const Questions: FC = () => (
   <div className={styles.text}>
-    <QA
-      question={"限界開発鯖ってなに?"}
-      answer={"限界開発鯖は高専生を中心に限界になった人々が限界状態で開発を行う Discord 鯖です。"}
-    />
-
-    <QA
-      question={"限界開発鯖の雰囲気が知りたい！"}
-      answer={
-        <>
-          こちらへどうぞ &rarr;{" "}
-          <ExternalLink href="https://twitter.com/search?q=%23限界開発鯖&src=typed_query">
-            #限界開発鯖
-          </ExternalLink>
-        </>
-      }
-    />
-    <QA
-      question={<>どうやって参加するの？</>}
-      answer={
-        <>
-          弊サーバーは、<span className={styles.color}>完全紹介制</span>です。
-          知り合いのメンバーから招待リンクを受け取ってください。
-        </>
-      }
-    />
+    {questionAnswers.map((props, i) => (
+      <QA key={i} {...props} />
+    ))}
   </div>
 );
