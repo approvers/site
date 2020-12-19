87012376191382b2c673134b26a2ad13d698c983
MikuroXina <ryosukadnak@gmail.com>
2020-12-19 20:41:54 +0900

    🎨 Fix page template

--- src/pages/link.tsx
+++ src/pages/link.tsx
@@ -1,18 +1,13 @@
-import { Header } from "../components/header";
 import { Layout } from "../components/layout";
 import { NextPage } from "next";
+import { Title } from "../components/title";
 import styles from "../scss/pages/link.module.scss";
 
 const LinkPage: NextPage = () => (
-  <>
-    <Layout pageName="限界開発鯖 - リンク">
-      <Header />
-      <main className={styles.joinPage}>
-        <h1 className={styles.title}>限界リンク集</h1>
-        <section className={styles.linkText}></section>
-      </main>
-    </Layout>
-  </>
+  <Layout pageName="限界開発鯖 - リンク">
+    <Title>限界リンク集</Title>
+    <section className={styles.linkText}></section>
+  </Layout>
 );
 
 export default LinkPage;
