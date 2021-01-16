05f0fad2256416ced718e17f4d0096f628229425
MikuroXina <ryosukadnak@gmail.com>
2021-01-16 22:29:18 +0900

    feat: Add support for footnote (#153)
    Change dependencies for this

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -2,27 +2,39 @@ import { Blog, getAllBlogInfos, getBlogFromId } from "../../lib/blog-fetch";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { DateString } from "../../components/date";
 import { Layout } from "../../components/layout";
-import Markdown from "markdown-to-jsx";
+import MarkdownIt from "markdown-it";
+import MarkdownItFootnote from "markdown-it-footnote";
+import MarkdownItFrontMatter from "markdown-it-front-matter";
 import emojify from "emojify-tag";
 import styles from "../../scss/pages/blog/markdown.module.scss";
+
+const md = MarkdownIt({
+  html: true,
+  linkify: true,
+})
+  .use(MarkdownItFrontMatter)
+  .use(MarkdownItFootnote);
 
 type BlogPostPageProps = {
   post: Blog;
 };
 
-const BlogPostPage: NextPage<BlogPostPageProps> = ({ post }) => (
-  <Layout pageName={`限界開発鯖 - ブログ - ${post.title}`}>
-    <h1 className={styles.title}>{post.title}</h1>
-    <div>
-      {post.author}
-      {" - "}
-      <DateString dateString={post.date} />
-    </div>
-    <div className={styles.markdown}>
-      <Markdown>{emojify`${post.content}`}</Markdown>
-    </div>
-  </Layout>
-);
+const BlogPostPage: NextPage<BlogPostPageProps> = ({ post }) => {
+  const bodyHtml = md.render(emojify`${post.content}`);
+  return (
+    <Layout pageName={`限界開発鯖 - ブログ - ${post.title}`}>
+      <h1 className={styles.title}>{post.title}</h1>
+      <div>
+        {post.author}
+        {" - "}
+        <DateString dateString={post.date} />
+      </div>
+      <div className={styles.markdown}>
+        <div dangerouslySetInnerHTML={{ __html: bodyHtml }} />
+      </div>
+    </Layout>
+  );
+};
 
 type BlogPostPagePathProps = {
   id: string;
