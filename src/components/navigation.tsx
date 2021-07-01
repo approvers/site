cbb91f78cb1e2e82a71deec81d86b91e55113aa7
Mikuroさいな <ryosukadnak@gmail.com>
2021-07-01 17:17:13 +0900

    ステージングの反映とステージング環境のテスト (#183)
    * style: Specify table style (#179)
    
    * style: Make dark main color more darker (#177)
    
    * ヘッダーの簡素化とナビメニューの追加 (#178)
    
    * feat: Add Navigation
    
    * feat: Simplify Header
    
    * feat: Apply on Layout
    
    * fix: Add transition and Adjust
    
    * style: Add margin-right
    
    * メニューのデザインの改善など (#180)
    
    * Make navigation aside
    
    * feat: Add color.back
    
    * Make up side menu
    
    * Fix to always show side menu on large screen
    
    * fix: Change scss code owner (#189)
    
    * fix: Fix to fixed position (#184)
    
    * Dependabot の警告に基づく依存関係の更新 (#193)
    
    * deps: Bump dependencies
    
    * fix: Fix dependencies for eslint
    
    * fix: Fix lint error
    
    * feat: Close nav menu on tap outside (#185)
    
    * TypeScript のバージョンをチルダで指定した (#194)
    
    * fix: Specify TypeScript version with tilde
    
    * deps: Update yarn.lock
    
    * 依存関係の修正 (#203)
    
    * deps: Update dependencies for security update
    
    * fix: Fix husky hooks setup
    
    * fix: Remove unneeded extends
    
    * Revert "fix: Fix husky hooks setup"
    
    This reverts commit 76c1e43fa9c8c1149e12e4112ccf2e8c3381abac.
    
    * fix: Add .husky
    
    * fix: Remove unused husky config
    
    * fix: Fix version specifying of postcss
    
    * fix: Use yarn over npx
    
    Co-authored-by: Shuntaro Nishizawa <me@shun.technology>
    
    Co-authored-by: Shuntaro Nishizawa <me@shun.technology>
    
    * fix: Update copyright year (#204)
    
    * deps: Update next for security update (#205)
    
    * fix: Fix data-showing condition (#202)
    
    * fix: Resolves #200 (#201)
    
    * Bump postcss from 7.0.35 to 7.0.36 (#210)
    
    Bumps [postcss](https://github.com/postcss/postcss) from 7.0.35 to 7.0.36.
    - [Release notes](https://github.com/postcss/postcss/releases)
    - [Changelog](https://github.com/postcss/postcss/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/postcss/postcss/compare/7.0.35...7.0.36)
    
    ---
    updated-dependencies:
    - dependency-name: postcss
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>
    
    * Bump trim-newlines from 3.0.0 to 3.0.1 (#207)
    
    Bumps [trim-newlines](https://github.com/sindresorhus/trim-newlines) from 3.0.0 to 3.0.1.
    - [Release notes](https://github.com/sindresorhus/trim-newlines/releases)
    - [Commits](https://github.com/sindresorhus/trim-newlines/commits)
    
    ---
    updated-dependencies:
    - dependency-name: trim-newlines
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>
    
    * Revert "fix: Fix data-showing condition (#202)" (#206)
    
    This reverts commit dc09675e7a51ccd76d0649cde9af6d069743b38b.
    
    Co-authored-by: Naoki Ikeguchi <root@siketyan.dev>
    Co-authored-by: Shuntaro Nishizawa <me@shun.technology>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/components/navigation.tsx
@@ -0,0 +1,56 @@
+import { FC, useCallback, useRef, useState } from "react";
+import { Button } from "./button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import styles from "../scss/components/navigation.module.scss";
+
+export interface LinksProps {
+  links: readonly Readonly<{
+    name: string;
+    url: string;
+  }>[];
+}
+
+const Links: FC<LinksProps> = ({ links }) => (
+  <nav className={styles.buttonWrapper}>
+    {links.map(({ name, url }) => (
+      <Button key={name}>
+        <Link href={url}>
+          <a>{name}</a>
+        </Link>
+      </Button>
+    ))}
+  </nav>
+);
+
+const NavButton: FC<{ onClick: () => void }> = ({ onClick }) => (
+  <button className={styles.navButton} onClick={onClick}>
+    <FontAwesomeIcon icon={faBars} />
+  </button>
+);
+
+export const Navigation: FC<LinksProps> = (props) => {
+  const [showingLinks, setShowingLinks] = useState(false);
+  const canceler = useRef<HTMLDivElement | null>(null);
+  const onCancel = useCallback(
+    (e: { target: EventTarget }) => e.target == canceler.current && setShowingLinks(() => false),
+    [canceler],
+  );
+
+  return (
+    <>
+      <NavButton onClick={() => setShowingLinks((v) => !v)} />
+      <div className={styles.navWrapper} data-showing={showingLinks}>
+        <Links {...props} />
+        <div
+          onTouchStart={onCancel}
+          onClick={onCancel}
+          className={styles.canceler}
+          ref={canceler}
+          data-showing={showingLinks}
+        />
+      </div>
+    </>
+  );
+};
