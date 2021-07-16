69d9625c058f4f4be8b122321e24f0913cd017f2
Mikuroさいな <ryosukadnak@gmail.com>
2021-07-16 19:35:19 +0900

    簡単なリファクタ (#215)
    * refactor: Fix plural form
    
    * refactor: Refactor gtag

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? "";
 
-export const pageview = (url: string): void => {
+export const notifyShowingPage = (url: string): void => {
   window.gtag("config", GOOGLE_ANALYTICS_ID, {
     page_path: url,
   });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
 import "../scss/global.scss";
-import * as gtag from "../lib/gtag";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { notifyShowingPage } from "../lib/gtag";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
 const Page = ({ Component, pageProps }: AppProps): JSX.Element => {
   const router = useRouter();
   useEffect(() => {
-    const handleRouteChange = (url: string): void => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("routeChangeComplete", notifyShowingPage);
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("routeChangeComplete", notifyShowingPage);
     };
   }, [router.events]);
   return (
