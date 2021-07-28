ac236214a2745d63442205063cf107fad32065c3
merunno <piki.0147@gmail.com>
2021-07-29 00:28:38 +0900

    :ambulance: module.scss のファイルを分ける

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,14 +3,14 @@ import { Layout } from "../components/layout";
 import { Title } from "../components/title";
 import { Subtitle } from "../components/title";
 import { NextPage } from "next";
-import styles from "../scss/pages/blog.module.scss";
+import styles from "../scss/pages/404.module.scss";
 
 const PageError: NextPage = () => (
   <Layout pageName="404 - Not found">
     <Title>404 Not found</Title>
     <Subtitle>ページが見つかりません。</Subtitle>
     <Link href="/">
-      <a className={styles.pageLink}>トップページへ戻る</a>
+      <a className={styles.topLink}>トップページへ戻る</a>
     </Link>
   </Layout>
 );
