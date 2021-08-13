e27afaf076f22c08d8fb742b0076a3f337537518
MikuroXina <ryosukadnak@gmail.com>
2021-08-13 15:59:52 +0900

    Revert "fix: Bring back Window typing"
    This reverts commit 0706ef459f41fab8dd86cf317cdd679417ca2cca.

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,18 +2,5 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-interface Window {
-  // pageview
-  gtag(type: "config", googleAnalyticsId: string, { page_path: string }): void;
-
-  // event
-  gtag(
-    type: "event",
-    eventAction: string,
-    fieldObject: {
-      event_label: string;
-      event_category: string;
-      value?: string;
-    },
-  ): void;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
