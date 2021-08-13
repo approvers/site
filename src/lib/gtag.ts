989a9aae756b07e8af8db2b6537bac570c494370
MikuroXina <ryosukadnak@gmail.com>
2021-08-13 12:38:55 +0900

    fix: Add typing

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,7 +1,9 @@
 export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? "";
 
 export const notifyShowingPage = (url: string): void => {
-  window.gtag("config", GOOGLE_ANALYTICS_ID, {
+  (
+    window as unknown as { gtag(event: "config", id: string, options: { page_path: string }): void }
+  ).gtag("config", GOOGLE_ANALYTICS_ID, {
     page_path: url,
   });
 };
@@ -14,7 +16,15 @@ type Event = {
 };
 
 export const event = ({ action, category, label, value }: Event): void => {
-  window.gtag("event", action, {
+  (
+    window as unknown as {
+      gtag(
+        event: "event",
+        id: string,
+        options: { event_category: string; event_label: string; value?: string },
+      ): void;
+    }
+  ).gtag("event", action, {
     event_category: category,
     event_label: label,
     value: value,
