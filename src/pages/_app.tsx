193a75057960ea7baa2132bd6b5a56ef1596a53c
MikuroXina <ryosukadnak@gmail.com>
2022-02-13 22:02:00 +0900

    Indent inline script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,16 +24,18 @@ const Page = ({ Component, pageProps }: AppProps): JSX.Element => {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
       />
-      <Script strategy="afterInteractive" id="send-ga">{`
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag("js", new Date());
-      gtag("config", "${GOOGLE_ANALYTICS_ID}", {
-        page_path: window.location.pathname,
-      });
-    `}</Script>
+      <Script strategy="afterInteractive" id="send-ga">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "${GOOGLE_ANALYTICS_ID}", {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
