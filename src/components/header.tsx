bebf758293834bb3246ec2a777d8bdadad896844
MikuroXina <ryosukadnak@gmail.com>
2022-02-13 22:31:58 +0900

    Specify width and height for img

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header = (): JSX.Element => (
   <header className={styles.headerLayout}>
     <Link href="/">
       <a className={styles.hero}>
-        <img src="/android-chrome-512x512.png" alt="Approversロゴ"></img>
+        <img src="/android-chrome-512x512.png" width={512} height={512} alt="Approversロゴ"></img>
         Approvers
       </a>
     </Link>
