2dd8a45f838d52e35dfbed019ef28b3458584aa6
MikuroXina <ryosukadnak@gmail.com>
2022-02-13 21:58:02 +0900

    Fix with next/script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../scss/global.scss";
+import { GOOGLE_ANALYTICS_ID, notifyShowingPage } from "../lib/google-analytics";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { notifyShowingPage } from "../lib/google-analytics";
+import Script from "next/script";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -19,6 +20,20 @@ const Page = ({ Component, pageProps }: AppProps): JSX.Element => {
         <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script strategy="afterInteractive" id="send-ga">{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "${GOOGLE_ANALYTICS_ID}", {
+        page_path: window.location.pathname,
+      });
+    `}</Script>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,31 +1,10 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { GOOGLE_ANALYTICS_ID } from "../lib/gtag";
-
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja">
-        <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag() {
-                  dataLayer.push(arguments);
-                }
-                gtag("js", new Date());
-                gtag("config", "${GOOGLE_ANALYTICS_ID}", {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
