ad75ee94c9f1980b142c74f25857052849ee50a3
Mikuroさいな <ryosukadnak@gmail.com>
2022-02-13 23:09:09 +0900

    fix: Google Analytics の注入処理の修正 (#242)
    * Update deps
    
    * Rename into google-analytics
    
    * Fix with next/script
    
    * Indent inline script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../scss/global.scss";
+import { GOOGLE_ANALYTICS_ID, notifyShowingPage } from "../lib/google-analytics";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { notifyShowingPage } from "../lib/gtag";
+import Script from "next/script";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -18,6 +19,22 @@ const Page = ({ Component, pageProps }: AppProps): JSX.Element => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script strategy="afterInteractive" id="send-ga">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "${GOOGLE_ANALYTICS_ID}", {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-import { GOOGLE_ANALYTICS_ID } from "../lib/gtag";
-
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
@@ -16,24 +14,6 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
             media="print"
-          />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag() {
-                  dataLayer.push(arguments);
-                }
-                gtag("js", new Date());
-                gtag("config", "${GOOGLE_ANALYTICS_ID}", {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
           />
         </Head>
         <body>
