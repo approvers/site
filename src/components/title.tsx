4893a6743ee7522f1a4826c9d65f3b22249dbe56
MikuroXina <ryosukadnak@gmail.com>
2022-02-28 16:23:29 +0900

    Fix spacing Title

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,7 +2,7 @@ import { Center, Heading } from "@chakra-ui/react";
 import type { ReactNode } from "react";
 
 export const Title = ({ children }: { children?: ReactNode }): JSX.Element => (
-  <Center p={8}>
+  <Center mt={8} mb={8}>
     <Heading as="h1" fontSize="2xl">
       {children}
     </Heading>
