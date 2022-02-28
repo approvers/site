228eec91790a801ca97104a615196c543f4079ba
MikuroXina <ryosukadnak@gmail.com>
2022-02-28 16:18:08 +0900

    Fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,13 @@
-import { Center, Link } from "@chakra-ui/react";
+import { Box, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 export const Footer = (): JSX.Element => (
-  <Center w="100%" p={4} borderTop="2px" borderColor="shadowed">
+  <Box textAlign="center" p={4} borderTop="2px" borderColor="shadowed">
     <NextLink href="/policy" passHref>
       <Link mr={2} fontStyle="normal" color="highlighted">
         プライバシーポリシー
       </Link>
     </NextLink>
     - (c) 2021 Approvers
-  </Center>
+  </Box>
 );
