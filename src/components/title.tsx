1e0ff670caaf632b3e9954f7af9303994fdcc82e
MikuroXina <ryosukadnak@gmail.com>
2022-03-01 19:14:59 +0900

    Make title more semantic

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,12 +1,10 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import type { ReactNode } from "react";
 
 export const Title = ({ children }: { children?: ReactNode }): JSX.Element => (
-  <Center mt={8} mb={8}>
-    <Heading as="h1" fontSize="2xl">
-      {children}
-    </Heading>
-  </Center>
+  <Heading mt={8} mb={8} as="h1" fontSize="2xl" w="100%" textAlign="center">
+    {children}
+  </Heading>
 );
 
 export const Subtitle = ({ children }: { children?: ReactNode }): JSX.Element => (
