6165a485e5fce77675e0f7a3dcc309c09c0140d2
Shuntaro Nishizawa <me@shun.technology>
2022-11-05 22:36:15 +0900

    chore: Add comment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    // NOTE: https://github.com/chakra-ui/chakra-ui/issues/6783#issuecomment-1283788233
     deps: {
       fallbackCJS: true,
     },
