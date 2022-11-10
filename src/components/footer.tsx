c8f98d8d0e6ffde15c1c928796717496595cd730
MikuroXina <ryosukadnak@gmail.com>
2022-11-10 13:46:01 +0900

    fix: Improve with yearContext

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,19 @@
 import { Box, Link } from "@chakra-ui/react";
-import NextLink from "next/link";
 
-export const Footer = (): JSX.Element => (
-  <Box as="footer" p={4} textAlign="center" borderColor="shadowed" borderTopWidth="2px">
-    <NextLink href="/policy" passHref>
-      <Link mr={2} color="highlighted" fontStyle="normal">
-        プライバシーポリシー
-      </Link>
-    </NextLink>
-    - (c) {new Date().getFullYear()} Approvers
-  </Box>
-);
+import NextLink from "next/link";
+import { useContext } from "react";
+import { yearContext } from "../lib/year-context";
+
+export const Footer = (): JSX.Element => {
+  const year = useContext(yearContext);
+  return (
+    <Box as="footer" p={4} textAlign="center" borderColor="shadowed" borderTopWidth="2px">
+      <NextLink href="/policy" passHref>
+        <Link mr={2} color="highlighted" fontStyle="normal">
+          プライバシーポリシー
+        </Link>
+      </NextLink>
+      - (c) {year} Approvers
+    </Box>
+  );
+};
