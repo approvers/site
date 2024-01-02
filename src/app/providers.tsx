579947c34eb5ab6e3e3998c429ef3413828e2258
MikuroXina <ryosukadnak@gmail.com>
2024-01-02 12:05:27 +0900

    Optimize framer motion

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LazyMotion, domAnimation } from "framer-motion";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./lib/theme";
 import { yearContext } from "./lib/year-context";
@@ -7,7 +8,9 @@ import { yearContext } from "./lib/year-context";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <yearContext.Provider value={new Date().getFullYear()}>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <LazyMotion features={domAnimation}>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </LazyMotion>
     </yearContext.Provider>
   );
 }
