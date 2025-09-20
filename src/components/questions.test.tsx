91db83c17e41c648c2bff680691d927af6ab1164
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-09-20 21:18:12 +0900

    Update tests

--- src/components/questions.test.tsx
+++ src/components/questions.test.tsx
@@ -1,8 +1,8 @@
 import { expect, it } from "vitest";
 import { Questions } from "./questions";
-import { render } from "../../utils/react-test";
+import { render } from "../utils/react-test";
 
 it("renders correctly", () => {
   const tree = render(<Questions />);
-  expect(tree.toJSON()).toMatchSnapshot();
+  expect(tree.container.innerHTML).toMatchSnapshot();
 });
--- src/components/sns-link.test.tsx
+++ src/components/sns-link.test.tsx
@@ -1,13 +1,13 @@
 import { expect, it } from "vitest";
 import { SNSLink } from "./sns-link";
-import { render } from "../../utils/react-test";
+import { render } from "../utils/react-test";
 
 it("renders twitter link correctly", () => {
-  const tree = render(<SNSLink type="twitter" url="https://twitter.com/MikurXina" />);
-  expect(tree.toJSON()).toMatchSnapshot();
+  const tree = render(<SNSLink type="twitter" name="MikuroXina" />);
+  expect(tree.container.innerHTML).toMatchSnapshot();
 });
 
 it("renders github link correctly", () => {
-  const tree = render(<SNSLink type="github" url="https://github.com/MikurXina" />);
-  expect(tree.toJSON()).toMatchSnapshot();
+  const tree = render(<SNSLink type="github" name="MikuroXina" />);
+  expect(tree.container.innerHTML).toMatchSnapshot();
 });
