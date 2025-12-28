2be3b4d83e64d96001358997ebfa40d18d5b5c19
Mikuroさいな <10331164+MikuroXina@users.noreply.github.com>
2025-12-28 10:50:14 +0900

    ci: Authorize github API with GITHUB_TOKEN (#523)

--- src/lib/blog/github.ts
+++ src/lib/blog/github.ts
@@ -2,10 +2,17 @@ import type { BlogRepository } from "../blog-fetch";
 
 const ENDPOINT = "https://api.github.com";
 const BLOG_PATH = "/repos/approvers/site-data/contents/data/blogs/";
+const TOKEN_ENV_KEY = "GITHUB_TOKEN";
 
 export const githubBlogRepo: BlogRepository = {
   blogFiles: async () => {
-    const res = await fetch(ENDPOINT + BLOG_PATH);
+    const githubToken = process.env[TOKEN_ENV_KEY];
+    const headers: HeadersInit = githubToken
+      ? {
+          Authorization: `Bearer ${githubToken}`,
+        }
+      : {};
+    const res = await fetch(new URL(BLOG_PATH, ENDPOINT), { headers });
     if (!res.ok) {
       throw new Error(await res.text());
     }
@@ -13,7 +20,9 @@ export const githubBlogRepo: BlogRepository = {
     const fileNames: { name: string }[] = await res.json();
     return Promise.all(
       fileNames.map(async ({ name: fileName }) => {
-        const res = await fetch(ENDPOINT + BLOG_PATH + fileName);
+        const res = await fetch(new URL(BLOG_PATH + fileName, ENDPOINT), {
+          headers,
+        });
         if (!res.ok) {
           throw new Error(await res.text());
         }
